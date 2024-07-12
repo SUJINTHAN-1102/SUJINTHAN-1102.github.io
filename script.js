a308d827cd525bea6938b655b40386444ae9a168
SUJINTHAN-1102 <sujinthanuthayakumar@gmail.com>
2024-07-12 22:36:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/*=================Typing animation ===================*/
+var typed=new Typed(".typing",{
+    strings:["","Flutter Developer","Web Developer","Grapic Designer", "Web Designer","Project Manager"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
